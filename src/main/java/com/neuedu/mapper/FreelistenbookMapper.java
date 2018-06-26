e8bfdf022f768537c69866c4f9cc5971ca3562f6
chouear <chouear@outlook.com>
2018-06-26 10:07:02 +0800

    增加一行注释于FreelistenbookMapper

--- src/main/java/com/neuedu/mapper/FreelistenbookMapper.java
+++ src/main/java/com/neuedu/mapper/FreelistenbookMapper.java
@@ -6,4 +6,7 @@ import java.util.ArrayList;
 
 public interface FreelistenbookMapper {
     public ArrayList<Freelistenbook> getFreelistenbook() throws Exception;
+
+    //////////////////////////////////////////////////////////
+    
 }
